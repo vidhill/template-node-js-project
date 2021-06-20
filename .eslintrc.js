28cf91bb7629c6f13978d88bad9940f1a55f9ae3
David Hill <dh366k@us.att.com>
2021-06-20 23:55:37 +0100

    Extend eslint config from preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,45 +4,8 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    plugins: ['prettier', 'import', 'unicorn'],
-    extends: ['eslint:recommended', 'plugin:node/recommended'],
+    extends: ['vidhill'],
     parserOptions: {
         ecmaVersion: 12,
     },
-    rules: {
-        'prettier/prettier': 'error',
-        'no-console': 'error',
-        'prefer-const': 'error',
-        'unicorn/filename-case': [
-            'error',
-            {
-                case: 'kebabCase',
-            },
-        ],
-        'unicorn/no-abusive-eslint-disable': 'error',
-        'unicorn/explicit-length-check': 'error',
-        'no-useless-rename': 'error',
-        'import/no-cycle': 'error',
-        'import/order': [
-            'error',
-            {
-                'newlines-between': 'always',
-            },
-        ],
-        'import/newline-after-import': 'error',
-        'import/group-exports': 'error',
-        'node/prefer-promises/fs': 'error',
-        'node/exports-style': ['error', 'module.exports'],
-    },
-    overrides: [
-        {
-            // unit test overrides
-            files: ['jest-config/*', '*.spec.js'],
-            plugins: ['jest'],
-            extends: ['plugin:jest/recommended'],
-            env: {
-                jest: true,
-            },
-        },
-    ],
 };
